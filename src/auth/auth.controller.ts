35f4c834d7c2d41d75bdc570d3b45c55f2770021
radenlarko <radenlarko@gmail.com>
2023-04-20 10:10:28 +0700

    Add route auth/signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() payload: AuthDto) {
+    return this.authService.signup(payload);
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
+
+  @Get('signout')
+  signout() {
+    return this.authService.signout();
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(payload: AuthDto) {
+    const { email, password } = payload;
+
+    const foundUser = await this.prisma.user.findUnique({ where: { email } });
+
+    if (foundUser) {
+      throw new BadRequestException('Email already exist');
+    }
+
+    const hashedPassword = await this.hashPassword(password);
+
+    await this.prisma.user.create({
+      data: {
+        email,
+        hashedPassword,
+      },
+    });
+
+    return { message: 'signup was successfull' };
+  }
+
+  async signin() {
+    return { message: 'signin was successfull' };
+  }
+
+  async signout() {
+    return { message: 'signout was successfull' };
+  }
+
+  async hashPassword(password: string) {
+    const saltOrRounds = 10;
+    const hashed = await bcrypt.hash(password, saltOrRounds);
+
+    return hashed;
+  }
+}
